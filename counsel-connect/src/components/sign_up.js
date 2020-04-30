2f2eeee33bcd25b665dcd9ec2fe14db6f4d23590
Olivia Otto <oliviaotto17@hotmail.com>
2020-04-30 16:46:04 -0400

    First screen of sign up process
    screen to choose between lawyer/survivor

--- counsel-connect/src/components/sign_up.js
+++ counsel-connect/src/components/sign_up.js
@@ -1,9 +1,64 @@
 import React, { useState } from "react";
-import { css } from "@emotion/core";
+import Container from "@material-ui/core/Container";
+import "./sign_up.css";
 import NavBar from "./sign_up_nav_bar.js";
 import SurvivorForm from "./sign_up_form_survivor.js";
 import LawyerForm from "./sign_up_form_lawyer.js";
 
+const TypeCard = (props) => {
+  return (
+    <Container className="one" maxWidth="sm">
+      <Container className="three" maxWidth="sm"></Container>
+      <Container className="four" maxWidth="sm">
+        <Container className="five" maxWidth="sm">
+          <div>I am a:</div>
+
+          <div className="seven">
+            <div
+              className="lawyerButton"
+              onClick={function () {
+                props.setIsFirstScreen(false);
+                props.setIsLawyer(true);
+              }}
+            >
+              LAWYER
+            </div>
+            <div
+              className="survivorButton"
+              onClick={function () {
+                props.setIsFirstScreen(false);
+                props.setIsLawyer(false);
+              }}
+            >
+              SURVIVOR
+            </div>
+          </div>
+        </Container>
+      </Container>
+    </Container>
+  );
+};
+
 export default function SignUp(props) {
-  return <NavBar setIsSignIn={props.setIsSignIn} />;
+  const [isFirstScreen, setIsFirstScreen] = useState(true);
+  const [isLawyer, setIsLawyer] = useState(true);
+
+  if (isFirstScreen) {
+    return (
+      <div className="screen">
+        <NavBar setIsSignIn={props.setIsSignIn} />
+        <TypeCard
+          setIsFirstScreen={setIsFirstScreen}
+          setIsLawyer={setIsLawyer}
+        />
+        <div className="footer"></div>
+      </div>
+    );
+  }
+
+  if (isLawyer) {
+    return <LawyerForm />;
+  }
+
+  return <SurvivorForm />;
 }
--- counsel-connect/src/components/sign_up_nav_bar.js
+++ counsel-connect/src/components/sign_up_nav_bar.js
@@ -2,10 +2,9 @@ import React from "react";
 import { makeStyles } from "@material-ui/core/styles";
 import AppBar from "@material-ui/core/AppBar";
 import Toolbar from "@material-ui/core/Toolbar";
+import Container from "@material-ui/core/Container";
 import Typography from "@material-ui/core/Typography";
 import Button from "@material-ui/core/Button";
-import IconButton from "@material-ui/core/IconButton";
-import MenuIcon from "@material-ui/icons/Menu";
 import { createMuiTheme } from "@material-ui/core/styles";
 import { ThemeProvider } from "@material-ui/styles";
 
@@ -28,16 +27,13 @@ const useStyles = makeStyles((theme) => ({
   menuButton: {
     marginRight: theme.spacing(),
   },
-  title: {
-    //flexGrow: 1,
-  },
 }));
 
 export default function ButtonAppBar(props) {
   const classes = useStyles();
 
   return (
-    <div className={classes.root}>
+    <Container className={classes.root} maxWidth="false">
       <ThemeProvider theme={themeA}>
         <AppBar color="primary" position="static">
           <Toolbar>
@@ -56,6 +52,6 @@ export default function ButtonAppBar(props) {
           </Toolbar>
         </AppBar>
       </ThemeProvider>
-    </div>
+    </Container>
   );
 }
