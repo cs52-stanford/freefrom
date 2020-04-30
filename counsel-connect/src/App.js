10eaa23f5063b2aeb198ab81e11df763a6456f57
Olivia Otto <oliviaotto17@hotmail.com>
2020-04-29 21:10:14 -0400

    Navigating to the sign up page
    I changed the app.js file so I could switch to the sign up page by clicking on "Don't have an account? Sign Up"

--- counsel-connect/src/App.js
+++ counsel-connect/src/App.js
@@ -1,15 +1,29 @@
-import React from "react";
+import React, { useState } from "react";
 import logo from "./logo.svg";
-import SignIn from "./components/sign_in.js"
+import SignIn from "./components/sign_in.js";
+import SignUp from "./components/sign_up.js";
 import "./App.css";
 
 function App() {
-  return (
-    // test comment -Olivia 2.0
-    <div className="App">
+  const [isSignIn, setIsSignIn] = useState(true);
+
+  // if isSignIn is true, display sign in page
+  if (isSignIn) {
+    return (
+      <div className="App">
         <div className="container">
-            <SignIn></SignIn>
+          <SignIn setIsSignIn={setIsSignIn} />
         </div>
+      </div>
+    );
+  }
+
+  // otherwise (if isSignIn is false), display sign up page
+  return (
+    <div className="App">
+      <div className="container">
+        <SignUp />
+      </div>
     </div>
   );
 }
--- counsel-connect/src/components/sign_in.js
+++ counsel-connect/src/components/sign_in.js
@@ -1,27 +1,27 @@
-import React from 'react';
-import Avatar from '@material-ui/core/Avatar';
-import Button from '@material-ui/core/Button';
-import CssBaseline from '@material-ui/core/CssBaseline';
-import TextField from '@material-ui/core/TextField';
-import FormControlLabel from '@material-ui/core/FormControlLabel';
-import Checkbox from '@material-ui/core/Checkbox';
-import Link from '@material-ui/core/Link';
-import Grid from '@material-ui/core/Grid';
-import Box from '@material-ui/core/Box';
-import LockOutlinedIcon from '@material-ui/icons/LockOutlined';
-import Typography from '@material-ui/core/Typography';
-import { makeStyles } from '@material-ui/core/styles';
-import Container from '@material-ui/core/Container';
+import React from "react";
+import Avatar from "@material-ui/core/Avatar";
+import Button from "@material-ui/core/Button";
+import CssBaseline from "@material-ui/core/CssBaseline";
+import TextField from "@material-ui/core/TextField";
+import FormControlLabel from "@material-ui/core/FormControlLabel";
+import Checkbox from "@material-ui/core/Checkbox";
+import Link from "@material-ui/core/Link";
+import Grid from "@material-ui/core/Grid";
+import Box from "@material-ui/core/Box";
+import LockOutlinedIcon from "@material-ui/icons/LockOutlined";
+import Typography from "@material-ui/core/Typography";
+import { makeStyles } from "@material-ui/core/styles";
+import Container from "@material-ui/core/Container";
 
 function Copyright() {
   return (
     <Typography variant="body2" color="textSecondary" align="center">
-      {'Copyright © '}
+      {"Copyright © "}
       <Link color="inherit" href="https://material-ui.com/">
         Your Website
-      </Link>{' '}
+      </Link>{" "}
       {new Date().getFullYear()}
-      {'.'}
+      {"."}
     </Typography>
   );
 }
@@ -29,16 +29,16 @@ function Copyright() {
 const useStyles = makeStyles((theme) => ({
   paper: {
     marginTop: theme.spacing(8),
-    display: 'flex',
-    flexDirection: 'column',
-    alignItems: 'center',
+    display: "flex",
+    flexDirection: "column",
+    alignItems: "center",
   },
   avatar: {
     margin: theme.spacing(1),
     backgroundColor: theme.palette.secondary.main,
   },
   form: {
-    width: '100%', // Fix IE 11 issue.
+    width: "100%", // Fix IE 11 issue.
     marginTop: theme.spacing(1),
   },
   submit: {
@@ -46,7 +46,7 @@ const useStyles = makeStyles((theme) => ({
   },
 }));
 
-export default function SignIn() {
+export default function SignIn(props) {
   const classes = useStyles();
 
   return (
@@ -102,7 +102,13 @@ export default function SignIn() {
               </Link>
             </Grid>
             <Grid item>
-              <Link href="#" variant="body2">
+              <Link
+                href="#"
+                variant="body2"
+                onClick={function () {
+                  props.setIsSignIn(false);
+                }}
+              >
                 {"Don't have an account? Sign Up"}
               </Link>
             </Grid>
