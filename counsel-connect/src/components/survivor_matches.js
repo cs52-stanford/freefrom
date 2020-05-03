22d964e2d097e55096d332079a4f3193342c7de9
alissav <63168432+alissav30@users.noreply.github.com>
2020-05-02 21:41:02 -0400

    survivor header and matches

--- counsel-connect/src/components/survivor_matches.js
+++ counsel-connect/src/components/survivor_matches.js
@@ -1,58 +1,11 @@
-import React from 'react';
-import { makeStyles } from '@material-ui/core/styles';
-import AppBar from '@material-ui/core/AppBar';
-import Toolbar from '@material-ui/core/Toolbar';
-import Typography from '@material-ui/core/Typography';
-import Button from '@material-ui/core/Button';
-import IconButton from '@material-ui/core/IconButton';
-import MenuIcon from '@material-ui/icons/Menu';
+import React, { Component } from 'react';
+import SurvivorHeader from "./survivor_header.js";
 
-const useStyles = makeStyles((theme) => ({
-  root: {
-    flexGrow: 1,
-  },
-  menuButton: {
-    marginRight: theme.spacing(2),
-  },
-  middleButton: {
-    marginRight: theme.spacing(0.5),
-  },
-  title: {
-    flexGrow: 1,
-  },
-}));
-
-export default function ButtonAppBar() {
-  const classes = useStyles();
-
-  return (
-    <container className={classes.root}>
-      <AppBar position="static">
-        <Toolbar>
-          <IconButton edge="start" className={classes.menuButton} color="inherit" aria-label="menu">
-            <MenuIcon />
-          </IconButton>
-          <Typography variant="h6" className={classes.title}>
-            Civil Seeker
-          </Typography>
-          <IconButton className={classes.middleButton} color="inherit">
-          </IconButton>
-          <Typography variant="h6" className={classes.title}>
-            Matches
-            </Typography>
-            <IconButton className={classes.middleButton} color="inherit">
-          </IconButton>
-          <Typography variant="h6" className={classes.title}>
-            Resources
-            </Typography>
-            <IconButton className={classes.middleButton} color="inherit">
-          </IconButton>
-          <Typography variant="h6" className={classes.title}>
-            Settings
-            </Typography>
-          <Button color="inherit">Logout</Button>
-        </Toolbar>
-      </AppBar>
-    </container>
-  );
+const SurvivorMatches = (props) => {
+    return (
+        <SurvivorHeader></SurvivorHeader>
+        
+    )
 }
+
+export default SurvivorMatches;
