27652d66cb800e139312cc7a22c4d274843eb6ef
mayaguzdar <55737002+mayaguzdar@users.noreply.github.com>
2020-05-03 08:55:50 -0700

    Update App.js
    updated changed name of component in app

--- counsel-connect/src/App.js
+++ counsel-connect/src/App.js
@@ -2,7 +2,7 @@ import React, { useState } from "react";
 import logo from "./logo.svg";
 import SignIn from "./components/sign_in.js";
 import SignUp from "./components/sign_up.js";
-import LawyerHome from "./components/lawyer_home";
+import LawyerMatches from "./components/lawyer_matches";
 import SurvivorMatches from "./components/survivor_matches";
 import "./App.css";
 
@@ -18,7 +18,7 @@ function App() {
 
   //if is lawyer, display lawyer homepage
   if (isLogIn == true && isLawyer == true) {
-    return <LawyerHome />;
+    return <LawyerMatches />;
   }
 
   // if isSignIn is true, display sign in page
