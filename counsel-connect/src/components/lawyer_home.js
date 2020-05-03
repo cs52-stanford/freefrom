f971c8477b2c4f6b0caad31c6f49dc64509c3218
mayaguzdar <55737002+mayaguzdar@users.noreply.github.com>
2020-05-03 08:55:29 -0700

    Delete lawyer_home.js
    updated changed name of component

--- counsel-connect/src/components/lawyer_home.js
+++
@@ -1,8 +0,0 @@
-import React, { Component } from "react";
-import LawyerHeader from "./survivor_header.js";
-
-const LawyerMatches = (props) => {
-  return <LawyerHeader></LawyerHeader>;
-};
-
-export default LawyerMatches;
