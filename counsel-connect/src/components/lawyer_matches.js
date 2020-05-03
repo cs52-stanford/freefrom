af981faa6cb6fdc518639df7c6caf7389569b9b5
mayaguzdar <55737002+mayaguzdar@users.noreply.github.com>
2020-05-03 08:55:16 -0700

    Create lawyer_matches.js
    changed the name of a component

---
+++ counsel-connect/src/components/lawyer_matches.js
@@ -0,0 +1,8 @@
+import React, { Component } from "react";
+import LawyerHeader from "./survivor_header.js";
+
+const LawyerMatches = (props) => {
+  return <LawyerHeader></LawyerHeader>;
+};
+
+export default LawyerMatches;
