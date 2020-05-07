84371b7d9a793eaeaf54439a3633d42f214209bf
mayaguzdar <55737002+mayaguzdar@users.noreply.github.com>
2020-05-06 22:54:50 -0700

    making colored circle for lawyer home

--- counsel-connect/src/components/lawyer_home.js
+++ counsel-connect/src/components/lawyer_home.js
@@ -1,6 +1,9 @@
 import React from "react";
+import "./lawyer_home.css";
+import Container from "@material-ui/core/Container";
+import { css } from "@emotion/core";
 
 const LawyerHome = (props) => {
-  return <p>home</p>;
+  return <div className="circle"></div>;
 };
 export default LawyerHome;
