658c31141365bf7277d5c4bbd3b29ba216c3c783
alissav <63168432+alissav30@users.noreply.github.com>
2020-05-14 11:36:30 -0400

    last resources note

--- src/components/survivor_resources.js
+++ src/components/survivor_resources.js
@@ -3,9 +3,9 @@ import React, { Component } from 'react';
 
 const SurvivorResources = (props) => {
     return (
-    <h5>
-        Resources will be added in the next iteration. This current version is just for testing the matches and setting portion of our application.
-    </h5>
+    <h2>
+        Resources will be added in the next iteration. This current version is just for testing the matches and settings portion of our application.
+    </h2>
     );
 }
  
