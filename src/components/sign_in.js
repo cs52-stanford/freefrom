c7261db13b5861871b8bdc1b1bea1a9f35703c93
alissav <63168432+alissav30@users.noreply.github.com>
2020-05-14 14:20:19 -0400

    counsel compass copyright

--- src/components/sign_in.js
+++ src/components/sign_in.js
@@ -45,7 +45,7 @@ function Copyright() {
     <Typography variant="body2" color="textSecondary" align="center">
       {"Copyright © "}
       <Link color="inherit" href="https://material-ui.com/">
-        Your Website
+        CounselCompass
       </Link>{" "}
       {new Date().getFullYear()}
       {"."}
