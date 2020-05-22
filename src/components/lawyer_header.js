a7ed08945f1b4c0a9205999671f1294bdd826a6e
mayaguzdar <55737002+mayaguzdar@users.noreply.github.com>
2020-05-22 16:19:43 -0700

    adding in resource page

---
+++ src/components/lawyer_header.js
@@ -0,0 +1,243 @@
+import React from "react";
+import clsx from "clsx";
+import { makeStyles, useTheme } from "@material-ui/core/styles";
+import Drawer from "@material-ui/core/Drawer";
+import CssBaseline from "@material-ui/core/CssBaseline";
+import AppBar from "@material-ui/core/AppBar";
+import Toolbar from "@material-ui/core/Toolbar";
+import List from "@material-ui/core/List";
+import Typography from "@material-ui/core/Typography";
+import Divider from "@material-ui/core/Divider";
+import IconButton from "@material-ui/core/IconButton";
+import Button from "@material-ui/core/Button";
+import MenuIcon from "@material-ui/icons/Menu";
+import ChevronLeftIcon from "@material-ui/icons/ChevronLeft";
+import ChevronRightIcon from "@material-ui/icons/ChevronRight";
+import { ListItemAvatar } from "@material-ui/core";
+import Logo from "./counselCompassLogo.png";
+import SettingsIcon from "./settings.png";
+import LogOutIcon from "./logout.png";
+import MatchesIcon from "./matches.png";
+import ListItem from "@material-ui/core/ListItem";
+import ConnectionsIcon from "./connections.png";
+import ListItemIcon from "@material-ui/core/ListItemIcon";
+import Avatar from "@material-ui/core/Avatar";
+import ListItemText from "@material-ui/core/ListItemText";
+
+const drawerWidth = 240;
+
+const useStyles = makeStyles((theme) => ({
+  root: {
+    display: "flex",
+    padding: theme.spacing(0, 0, 4),
+  },
+  appBar: {
+    transition: theme.transitions.create(["margin", "width"], {
+      easing: theme.transitions.easing.sharp,
+      duration: theme.transitions.duration.leavingScreen,
+    }),
+    backgroundColor: "#e1f5fe",
+    color: "#000000",
+  },
+  appBarShift: {
+    width: `calc(100% - ${drawerWidth}px)`,
+    marginLeft: drawerWidth,
+    transition: theme.transitions.create(["margin", "width"], {
+      easing: theme.transitions.easing.easeOut,
+      duration: theme.transitions.duration.enteringScreen,
+    }),
+  },
+  menuButton: {
+    marginRight: theme.spacing(2),
+    color: "#757575",
+  },
+  hide: {
+    display: "none",
+  },
+  drawer: {
+    width: drawerWidth,
+    flexShrink: 0,
+  },
+  drawerPaper: {
+    width: drawerWidth,
+    backgroundColor: "#e1f5fe",
+  },
+  drawerHeader: {
+    display: "flex",
+    alignItems: "center",
+    padding: theme.spacing(0, 1),
+    // necessary for content to be below app bar
+    ...theme.mixins.toolbar,
+    justifyContent: "flex-end",
+  },
+  content: {
+    flexGrow: 1,
+    padding: theme.spacing(3),
+    transition: theme.transitions.create("margin", {
+      easing: theme.transitions.easing.sharp,
+      duration: theme.transitions.duration.leavingScreen,
+    }),
+    marginLeft: -drawerWidth,
+  },
+  contentShift: {
+    transition: theme.transitions.create("margin", {
+      easing: theme.transitions.easing.easeOut,
+      duration: theme.transitions.duration.enteringScreen,
+    }),
+    marginLeft: 0,
+  },
+  box: {
+    display: "flex",
+    backgroundColor: "inherit",
+    width: "30%",
+    height: "30%",
+  },
+}));
+
+export default function PersistentDrawerLeft(props) {
+  const classes = useStyles();
+  const theme = useTheme();
+  const [open, setOpen] = React.useState(false);
+
+  const handleDrawerOpen = () => {
+    setOpen(true);
+  };
+
+  const handleDrawerClose = () => {
+    setOpen(false);
+  };
+
+  return (
+    <div className={classes.root}>
+      <CssBaseline />
+      <AppBar
+        position="fixed"
+        className={clsx(classes.appBar, {
+          [classes.appBarShift]: open,
+        })}
+      >
+        <Toolbar>
+          <IconButton
+            color="inherit"
+            aria-label="open drawer"
+            edge="start"
+            onClick={handleDrawerOpen}
+            className={classes.menuButton}
+          >
+            <MenuIcon />
+          </IconButton>
+          <Button>
+            <Avatar
+              className={classes.box}
+              src={Logo}
+              variant="square"
+            ></Avatar>
+          </Button>
+        </Toolbar>
+      </AppBar>
+      <Drawer
+        className={classes.drawer}
+        variant="persistent"
+        anchor="left"
+        open={open}
+        classes={{
+          paper: classes.drawerPaper,
+        }}
+      >
+        <div className={classes.drawerHeader}>
+          <IconButton onClick={handleDrawerClose}>
+            {theme.direction === "ltr" ? (
+              <ChevronLeftIcon />
+            ) : (
+              <ChevronRightIcon />
+            )}
+          </IconButton>
+        </div>
+        <Divider />
+        <List>
+          <ListItem
+            button
+            key="matches"
+            alignItems="center"
+            onClick={() => {
+              props.setIsHomeScreen(true);
+              props.setIsConnectionsScreen(false);
+              props.setIsSettingsScreen(false);
+            }}
+          >
+            <ListItemAvatar>
+              <Avatar src={MatchesIcon} variant="square"></Avatar>
+            </ListItemAvatar>
+            <ListItemText primary="Matches"></ListItemText>
+          </ListItem>
+          <ListItem
+            button
+            key="connections"
+            alignItems="center"
+            onClick={() => {
+              props.setIsHomeScreen(false);
+              props.setIsConnectionsScreen(true);
+              props.setIsSettingsScreen(false);
+            }}
+          >
+            <ListItemAvatar>
+              <Avatar src={ConnectionsIcon} variant="square"></Avatar>
+            </ListItemAvatar>
+            <ListItemText primary="Connections"></ListItemText>
+          </ListItem>
+          <ListItem
+            button
+            key="settings"
+            alignItems="center"
+            onClick={() => {
+              props.setIsHomeScreen(false);
+              props.setIsConnectionsScreen(false);
+              props.setIsSettingsScreen(true);
+            }}
+          >
+            <ListItemAvatar>
+              <Avatar src={ConnectionsIcon} variant="square"></Avatar>
+            </ListItemAvatar>
+            <ListItemText primary="Connections"></ListItemText>
+          </ListItem>
+          <ListItem
+            button
+            key="settings"
+            alignItems="center"
+            onClick={() => {
+              props.setIsHomeScreen(false);
+              props.setIsConnectionsScreen(false);
+              props.setIsSettingsScreen(false);
+              props.setIsResourceScreen(true);
+            }}
+          >
+            <ListItemAvatar>
+              <Avatar src={SettingsIcon} variant="square"></Avatar>
+            </ListItemAvatar>
+            <ListItemText primary="Settings"></ListItemText>
+          </ListItem>
+          <ListItem
+            button
+            key="logout"
+            alignItems="center"
+            onClick={() => {
+              props.setIsLogIn(false);
+            }}
+          >
+            <ListItemAvatar>
+              <Avatar src={LogOutIcon} variant="square"></Avatar>
+            </ListItemAvatar>
+            <ListItemText primary="Log Out"></ListItemText>
+          </ListItem>
+        </List>
+      </Drawer>
+      <main
+        className={clsx(classes.content, {
+          [classes.contentShift]: open,
+        })}
+      >
+        <div className={classes.drawerHeader} />
+      </main>
+    </div>
+  );
+}
---
+++ src/components/lawyer_matches.js
@@ -0,0 +1,192 @@
+import React, { useState } from "react";
+import LawyerHome from "./lawyer_home.js";
+import LawyerConnections from "./lawyers_messages_sent.js";
+import LawyerHeader from "./lawyer_header.js";
+import LawyerSettings from "./lawyer_settings";
+import ReachOut from "./lawyer_reach_out";
+import LawyerResource from "./lawyer_resource.js";
+
+const statuses = ["new!", "new!", "new!", "new!", "new!", "new!"];
+
+const LawyerMatches = (props) => {
+  const [isHomeScreen, setIsHomeScreen] = useState(true);
+  const [isConfirmScreen, setIsConfirmScreen] = useState(false);
+  const [isConnectionsScreen, setIsConnectionsScreen] = useState(false);
+  const [isSettingsScreen, setIsSettingsScreen] = useState(false);
+  const [viewProfile, setViewProfile] = useState(false);
+  const [survivorProfile, setSurvivorProfile] = useState([]);
+  const [survivorName, setSurvivorName] = useState("name");
+  const [survivorImage, setSurvivorImage] = useState("image");
+  const [lawyerResource, setLawyerResource] = useState(false);
+
+  const [unsentSurvivors, setUnsentSurvivors] = useState([0, 1, 2, 3, 4, 5]);
+  const [sentSurvivors, setSentSurvivors] = useState([]);
+  const [survivorIndex, setSurvivorIndex] = useState(0);
+
+  const survivorNames = [
+    "Alias 1",
+    "Alias 2",
+    "Alias 3",
+    "Alias 4",
+    "Alias 5",
+    "Alias 6",
+  ];
+  const survivorPhotos = [
+    "https://i.guim.co.uk/img/media/d0105731685e5b2b3daecf2fa00c9affaba832f1/0_0_2560_1536/master/2560.jpg?width=700&quality=85&auto=format&fit=max&s=6542f6e0d27c640c50459a8cf09941c4",
+    "https://pbs.twimg.com/media/EYUEhvDXgAINxfT?format=png&name=small",
+    "https://pbs.twimg.com/media/EYT23zZWAAEyGsJ?format=png&name=small",
+    "https://pbs.twimg.com/media/EYS_8TLXsAAo8W9?format=png&name=small",
+    "https://pbs.twimg.com/media/EYSJFcdWkAA5Lmu?format=png&name=small",
+    "https://pbs.twimg.com/media/EYNa4qyXQAApT6i?format=png&name=small",
+  ];
+
+  function setStatus(index, message) {
+    statuses[index] = message;
+  }
+
+  if (isHomeScreen === true) {
+    if (viewProfile === false) {
+      return (
+        <div className="lawyerhome">
+          <LawyerHeader
+            setIsHomeScreen={setIsHomeScreen}
+            setIsConnectionsScreen={setIsConnectionsScreen}
+            setIsSettingsScreen={setIsSettingsScreen}
+            setIsLogIn={props.setIsLogIn}
+            setIsLawyerResource={setIsLawyerResource}
+          />
+          <LawyerHome
+            viewProfile={viewProfile}
+            setViewProfile={setViewProfile}
+            setSurvivorImage={setSurvivorImage}
+            setSurvivorName={setSurvivorName}
+            setSurvivorProfile={setSurvivorProfile}
+            survivorName={survivorName}
+            survivorImage={survivorImage}
+            survivorProfile={survivorProfile}
+            isConfirmScreen={isConfirmScreen}
+            setIsConfirmScreen={setIsConfirmScreen}
+            survivorPhotos={survivorPhotos}
+            survivorNames={survivorNames}
+            unsentSurvivors={unsentSurvivors}
+            sentSurvivors={sentSurvivors}
+            setStatus={setStatus}
+            statuses={statuses}
+            setUnsentSurvivors={setUnsentSurvivors}
+            unsentSurvivors={unsentSurvivors}
+            setSentSurvivors={setSentSurvivors}
+            sentSurvivors={sentSurvivors}
+            survivorIndex={survivorIndex}
+            setSurvivorIndex={setSurvivorIndex}
+          />
+        </div>
+      );
+    } else {
+      return (
+        <div className="lawyerhome">
+          <LawyerHeader
+            setIsHomeScreen={setIsHomeScreen}
+            setIsConnectionsScreen={setIsConnectionsScreen}
+            setIsSettingsScreen={setIsSettingsScreen}
+            setIsLogIn={props.setIsLogIn}
+            setIsLawyerResource={setIsLawyerResource}
+          />
+          <ReachOut
+            viewProfile={viewProfile}
+            setViewProfile={setViewProfile}
+            setSurvivorImage={setSurvivorImage}
+            setSurvivorName={setSurvivorName}
+            setSurvivorProfile={setSurvivorProfile}
+            survivorName={survivorName}
+            survivorImage={survivorImage}
+            survivorProfile={survivorProfile}
+            isConfirmScreen={isConfirmScreen}
+            setIsConfirmScreen={setIsConfirmScreen}
+            survivorPhotos={survivorPhotos}
+            survivorNames={survivorNames}
+            unsentSurvivors={unsentSurvivors}
+            sentSurvivors={sentSurvivors}
+            setStatus={setStatus}
+            statuses={statuses}
+            setUnsentSurvivors={setUnsentSurvivors}
+            unsentSurvivors={unsentSurvivors}
+            setSentSurvivors={setSentSurvivors}
+            sentSurvivors={sentSurvivors}
+            survivorIndex={survivorIndex}
+            setSurvivorIndex={setSurvivorIndex}
+          />
+        </div>
+      );
+    }
+  }
+
+  if (isConnectionsScreen === true) {
+    return (
+      <div className="lawyerconnections">
+        <LawyerHeader
+          setIsHomeScreen={setIsHomeScreen}
+          setIsConnectionsScreen={setIsConnectionsScreen}
+          setIsSettingsScreen={setIsSettingsScreen}
+          setIsLogIn={props.setIsLogIn}
+        />
+        <LawyerConnections
+          viewProfile={viewProfile}
+          setViewProfile={setViewProfile}
+          setSurvivorImage={setSurvivorImage}
+          setSurvivorName={setSurvivorName}
+          setSurvivorProfile={setSurvivorProfile}
+          survivorName={survivorName}
+          survivorImage={survivorImage}
+          survivorProfile={survivorProfile}
+          isConfirmScreen={isConfirmScreen}
+          setIsConfirmScreen={setIsConfirmScreen}
+          survivorPhotos={survivorPhotos}
+          survivorNames={survivorNames}
+          unsentSurvivors={unsentSurvivors}
+          sentSurvivors={sentSurvivors}
+          setStatus={setStatus}
+          statuses={statuses}
+          setUnsentSurvivors={setUnsentSurvivors}
+          unsentSurvivors={unsentSurvivors}
+          setSentSurvivors={setSentSurvivors}
+          sentSurvivors={sentSurvivors}
+          survivorIndex={survivorIndex}
+          setSurvivorIndex={setSurvivorIndex}
+        />
+      </div>
+    );
+  }
+
+  if (isSettingsScreen === true) {
+    return (
+      <div className="lawyersettings">
+        <LawyerHeader
+          setIsHomeScreen={setIsHomeScreen}
+          setIsConnectionsScreen={setIsConnectionsScreen}
+          setIsSettingsScreen={setIsSettingsScreen}
+          setIsLogIn={props.setIsLogIn}
+        />
+        <LawyerSettings
+          setName={props.setName}
+          setGender={props.setGender}
+          setEmail={props.setEmail}
+          setPassword={props.setPassword}
+          name={props.name}
+          gender={props.gender}
+          email={props.email}
+          password={props.password}
+          setPracticeCounty={props.setPracticeCounty}
+          practiceCounty={props.practiceCounty}
+          experience={props.experience}
+          setExperience={props.setExperience}
+          setCompensationRequest={props.setCompensationRequest}
+          compensationRequest={props.compensationRequest}
+          setNumNotifications={props.setNumNotifications}
+          numNotifications={props.numNotifications}
+        />
+      </div>
+    );
+  }
+};
+
+export default LawyerMatches;
